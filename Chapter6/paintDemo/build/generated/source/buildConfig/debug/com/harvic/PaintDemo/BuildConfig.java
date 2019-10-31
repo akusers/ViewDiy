5d044bb5a5aa5e0ef43b16e59c53780cdccfd685
zsc <732116857@qq.com>
2019-10-31 21:31:41 +0800

    添加第六、七、八章源码

---
+++ Chapter6/paintDemo/build/generated/source/buildConfig/debug/com/harvic/PaintDemo/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.harvic.PaintDemo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.harvic.PaintDemo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
